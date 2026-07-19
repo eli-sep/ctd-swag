d9822656225efc57f663e4fd2f053d35c3f54ed8
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-07-19 22:18:34 +0200

    Lesson 3 - Adding Variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import ctdLogo from './assets/icons/mono-blue-logo.svg';
+import ctdLogo from './assets/mono-blue-logo.svg';
 import './App.css';
 
+const message = 'Coming Soon...'; //This is outside the function definition for App
+
 function App() {
+  const title = ' CTD Swag'; // This is inside the Component before the return
   return (
     <div className="coming-soon">
-      <h1>CTD Swag</h1>
+      <h1>{title}</h1> {/* `title` inserted into heading */}
       <div style={{ height: 100, width: 100 }}>
         <img src={ctdLogo} alt="Code The Dream Logo" />
       </div>
-      <h2>Coming Soon...</h2>
+      <h2>{message}</h2> {/* `message` inserted into heading */}
     </div>
   );
 }
