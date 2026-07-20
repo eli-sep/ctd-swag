9102cd8dab2e3b34faaf2fbf66836406a168d3ea
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-07-20 09:07:08 +0200

    Lesson 3 - Refactored code to create 3 components to and added them to App.

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,16 @@ import { useState } from 'react';
 import ctdLogo from './assets/mono-blue-logo.svg';
 import './App.css';
 import inventoryData from './assets/inventory.json';
+import Header from './Header';
+import ProductList from './ProductList';
 
 function App() {
   const [inventory, setInventory] = useState(inventoryData.inventory);
 
   return (
     <main>
-      <div className="coming-soon">
-        <h1>CTD Swag</h1>
-        <div style={{ height: 100, width: 100 }}>
-          <img src={ctdLogo} alt="Code The Dream Logo" />
-        </div>
-      </div>
-      <ul>
-        {inventory.map((item) => {
-          return (
-            <li key={item.id}>
-              <div className="itemCard">
-                <h2>{item.baseName}</h2>
-                <p>{item.baseDescription}</p>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      <Header />
+      <ProductList inventory={inventory} />
     </main>
   );
 }
